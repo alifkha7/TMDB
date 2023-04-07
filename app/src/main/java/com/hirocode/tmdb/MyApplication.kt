e5dff0469631472a534cb4f7a254297ce0b94250
Alif Khaerudin <alifkha7@live.com>
2023-04-07 20:33:28 +0700

    Add initialization koin

---
+++ app/src/main/java/com/hirocode/tmdb/MyApplication.kt
@@ -0,0 +1,26 @@
+package com.hirocode.tmdb
+
+import android.app.Application
+import com.hirocode.tmdb.di.networkModule
+import com.hirocode.tmdb.di.repositoryModule
+import com.hirocode.tmdb.di.useCaseModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin{
+            androidLogger()
+            androidContext(this@MyApplication)
+            modules(
+                listOf(
+                    networkModule,
+                    repositoryModule,
+                    useCaseModule,
+                )
+            )
+        }
+    }
+}
