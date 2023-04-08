7d38e026a4ae5ae7517d93c00e466faba7f8fe4b
Alif Khaerudin <alifkha7@live.com>
2023-04-08 14:21:07 +0700

    Add viewmodel module

--- app/src/main/java/com/hirocode/tmdb/MyApplication.kt
+++ app/src/main/java/com/hirocode/tmdb/MyApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.hirocode.tmdb.di.networkModule
 import com.hirocode.tmdb.di.repositoryModule
 import com.hirocode.tmdb.di.useCaseModule
+import com.hirocode.tmdb.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -19,6 +20,7 @@ class MyApplication : Application() {
                     networkModule,
                     repositoryModule,
                     useCaseModule,
+                    viewModelModule
                 )
             )
         }
--- app/src/main/java/com/hirocode/tmdb/di/AppModule.kt
+++ app/src/main/java/com/hirocode/tmdb/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.hirocode.tmdb.di
 
+import com.hirocode.tmdb.MainViewModel
 import com.hirocode.tmdb.data.repository.MoviesRepositoryImpl
 import com.hirocode.tmdb.data.source.MoviesDataSource
 import com.hirocode.tmdb.data.source.MoviesDataSourceImpl
@@ -8,6 +9,7 @@ import com.hirocode.tmdb.domain.repository.MoviesRepository
 import com.hirocode.tmdb.domain.usecase.GetPopularUseCase
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -38,4 +40,8 @@ val repositoryModule = module {
 
 val useCaseModule = module {
     factory { GetPopularUseCase(get()) }
+}
+
+val viewModelModule = module {
+    viewModel { MainViewModel(get()) }
 }
